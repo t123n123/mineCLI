721e6b4d4935f6dfffed30eae352a34d1077cafe
t123n123 <spineim22@gmail.com>
2023-05-17 22:40:24 +0200

    Simple window using cursive

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use cursive::views::{Dialog, TextView};
+
 fn main() {
+    let mut siv = cursive::default();
+
+    siv.add_layer(
+        Dialog::around(TextView::new("mineTerm"))
+            .title("mineTerm")
+            .button("Quit", |s| s.quit()),
+    );
+
+    siv.run();
+
     println!("Hello, world!");
 }
